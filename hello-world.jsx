e6eaedfd4f7f88e840bd97d3147ec97780ce01e0
Joacim Löwgren <joacim.lowgren@gmail.com>
2015-03-04 13:40:55 +0100

    First commit

---
+++ hello-world.jsx
@@ -0,0 +1,14 @@
+/* jshint globalstrict: true, esnext: true, browser: true */
+
+'use strict';
+
+import React from 'react';
+
+class HelloWorld extends React.Component {
+  render() {
+    return <p>Hello, world!</p>;
+  }
+}
+
+export default HelloWorld;
+
---
+++ index.jsx
@@ -0,0 +1,12 @@
+/* jshint globalstrict: true, esnext: true, browser: true */
+
+'use strict';
+
+import React from 'react';
+import HelloWorld from './hello-world';
+
+React.render(
+  <HelloWorld />,
+  document.getElementById('body')
+);
+
