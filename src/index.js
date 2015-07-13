89648e62367bf4ff11e9f6fbd28a770b30fc5244
Joacim Löwgren <joacim.lowgren@gmail.com>
2015-07-13 09:02:55 +0200

    Stick to naming convention

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import React from 'react';
-import HelloWorld from './components/hello-world';
+import HelloWorld from './components/HelloWorld';
 
 React.render(
   <HelloWorld />,
